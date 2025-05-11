e67e23a3771a6fa6e7482cd534731b1d05ad02d1
William Torres <ts25003@ues.edu.sv>
2025-05-10 22:36:53 -0600

    test

---
+++ src/main/java/com/iis/ts25003/demo_ts25003/App.java
@@ -0,0 +1,17 @@
+package com.iis.ts25003.demo_ts25003;
+
+/**
+ * Hello world!
+ */
+public class App 
+{
+	public String Hello() {
+		return "Hello World";
+	}
+	
+    public static void main(String[] args) 
+    {
+    	App hw = new App();
+        System.out.println(hw.Hello());
+    }
+}
---
+++ src/test/java/com/iis/ts25003/demo_ts25003/AppTest.java
@@ -0,0 +1,19 @@
+package com.iis.ts25003.demo_ts25003;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHello() {
+        assertTrue(true);
+    }
+}
